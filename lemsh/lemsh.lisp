1d15727dfa93bf55f192704f3e3115ff18b5e8de
homma <daisuke.homma@gmail.com>
2018-10-21 15:18:39 +0900

    committed on 2018/10/21 15:18.

---
+++ lemsh/lemsh.lisp
@@ -0,0 +1,96 @@
+(defpackage lemsh
+  (:use cl lem lem-base)
+  (:export start stop pwrite))
+
+(in-package lemsh)
+
+(defvar *proc*)
+(defvar *p-in*)
+(defvar *p-out*)
+
+(defvar *timeout* 0.05)
+(defvar *interval* 0.01)
+
+;;; start
+
+(defun create-process (command)
+  (setq *proc* (uiop:launch-program command
+                                    :input :stream
+                                    :output :stream))
+  (setq *p-in* (uiop:process-info-input *proc*))
+  (setq *p-out* (uiop:process-info-output *proc*)))
+
+(defun start (command)
+  (create-process command))
+
+;;; stop
+
+(defun stop-process ()
+  (uiop:close-streams *proc*)
+  (uiop:terminate-process *proc*)
+  (uiop:wait-process *proc*))
+
+(defun stop ()
+  (stop-process))
+
+;;; pwrite
+
+(defun listen-with-timeout (timeout interval in)
+  (if (< timeout 0)
+      nil
+      (if (listen in)
+          t
+          (progn
+            (sleep interval)
+            (listen-with-timeout (- timeout interval) interval in)))))
+
+(defun read-all (in)
+  (labels ((%read-all (acc in)
+             (let ((timeout *timeout*)
+                   (interval *interval*))
+               (if (listen-with-timeout timeout interval in)
+                   (%read-all (cons (read-line in) acc) in)
+                   acc))))
+    (write-to-string (%read-all nil in))))
+
+(defun write-and-flush (str out)
+  (progn
+    (write-line str out)
+    (finish-output out)))
+
+(defun write-buffer (str)
+  (let ((point (current-point)))
+    (insert-character point #\newline)
+    (insert-string point str)))
+
+(defun pwrite (str)
+  (write-and-flush str *p-in*)
+  (write-buffer (read-all *p-out*)))
+
+;;; test
+
+;; bc -lq
+(defun test-bc ()
+  ;; create sub process
+  (start "bc -lq")
+
+  ;; interact with it
+  (pwrite "1 * 2 * 3 * 4")
+
+  ;; quit
+  (pwrite "quit")
+  (stop)
+)
+
+;; sh
+(defun test-sh ()
+  ;; create sub process
+  (start "sh")
+
+  ;; interact with it
+  (pwrite "ls -l")
+
+  ;; quit
+  (pwrite "exit")
+  (stop)
+)
